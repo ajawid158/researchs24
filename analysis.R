28102719241aa86e6708c9f8aa7f758a744d4305
ajawid158 <ajawid@auaf.edu.af>
2024-07-11 19:52:37 +0430

    r11724

--- analysis.R
+++ analysis.R
@@ -1,9 +1,62 @@
 library(dplyr)
+library(ggplot2)
 
-d=read.csv("examdata.csv")
-View(d)
 
-sd(d$Oral.Exam)
-mean(d$Discussions)
-mean(d$Edpuzzle)
-sd(d$Online.Quiz)
+dt=read.csv("examdata.csv")
+
+
+summarise(dt, mean(Online.Quiz), mean(Project), mean(Discussions), 
+          mean(Edpuzzle), mean(Oral.Exam))
+
+
+d=read.csv("assessment.csv")
+head(d)
+
+AsT=c()
+for (i in 1:length(d$AssType)) {
+  if(d$AssType[i]==0){
+    AsT[i]="Dis"
+  } else if (d$AssType[i]==1){
+    AsT[i]="EdP"
+  } else if (d$AssType[i]==2){
+    AsT[i]="Qu"
+  } else {
+    AsT[i]="OE"
+  }
+  
+  
+}
+
+head(AsT)
+
+d=cbind(d, AsT)
+head(d)
+
+
+boxplot(Score~AsT,
+        data=d,
+        col="gray",
+        horizontal = T, 
+        xlab="",
+        ylab = "")
+
+stripchart(Score~AsT,
+           data = d,
+           method = 'jitter', 
+           pch=20,
+           col=2:5,
+           add = TRUE)
+
+g0=ggplot(d, aes(x=Score, color=AsT))
+g0+geom_density()+
+  theme_classic()+
+  xlim(10,115)+
+  theme(plot.title = element_text(face="bold",
+                                  hjust = .5), 
+        axis.title.x = element_text(),
+        axis.title.y = element_text(), 
+        legend.title = element_text(color='blue'),
+        legend.position = "top")+
+  xlab("")+
+  ylab("")
+
